f8eed45632cbf6f6ffde70be5fa560539fc050a9
Jose Antonio Liebana Delgado <jliebanadelgado@gmail.com>
2024-07-16 22:42:41 +0200

    A new project has been added: I am poor

---
+++ Curso Swift/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Jose Antonio Liebana Delgado on 16/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
